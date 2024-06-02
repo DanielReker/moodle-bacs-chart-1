b54f7ae0747da8c97287b05f31b1834be468f615
Daniel Reker <sasha29072004@gmail.com>
2024-06-03 01:33:28 +0400

    Fix coding style

--- chart1.php
+++ chart1.php
@@ -1,12 +1,39 @@
 <?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This page displays a histogram showing BACS submits amount distribution during the day.
+ * Submits can be filtered by a simple form by course and time period.
+ *
+ * @package    mod_bacs
+ * @copyright  2024 BACS
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+use core\chart_bar;
+use core\chart_series;
+
+require_once(dirname(__FILE__, 2) . '/config.php');
 
 global $OUTPUT, $PAGE, $DB, $CFG;
 
-require_once(dirname(__FILE__, 2) . '/config.php');
-require_once("{$CFG->libdir}/formslib.php");
+require_once("$CFG->libdir/formslib.php");
 
 
-// Set up page
+// Set up page.
 $PAGE->set_url(new moodle_url('/bacs_charts/chart1.php'));
 $PAGE->set_context(context_system::instance());
 $PAGE->set_heading("Submits amount distribution during the day");
@@ -15,133 +42,176 @@ $PAGE->set_pagelayout("standard");
 
 require_login();
 
-
-// Filter form
+/**
+ * Filter form that allows user to select course and time period
+ */
 class filter_form extends moodleform {
-    private DateTime $default_from, $default_to;
-    private $default_course_id = 'all';
+    /**
+     * Default "from" time
+     * @var DateTime
+     */
+    private DateTime $defaultfrom;
 
-    function __construct()
-    {
-        // Setting default filter values
-        $this->default_from = new DateTime("now", core_date::get_server_timezone_object());
-        $this->default_from->setTime(0, 0, 0); // From the start of today
+    /**
+     * Default "to" time
+     * @var DateTime
+     */
+    private DateTime $defaultto;
 
-        $this->default_to = new DateTime("now", core_date::get_server_timezone_object()); // Till now
-        $ceil_seconds = 60 - (int)$this->default_to->format("s");
-        $this->default_to->add(new DateInterval("PT{$ceil_seconds}S")); // Ceil to nearest minute
+    /**
+     * Default course ID
+     * @var string
+     */
+    private string $defaultcourseid = 'all';
+
+    /**
+     * This overridden constructor calculates default filter values and then calls parent constructor
+     * @throws Exception
+     */
+    public function __construct() {
+        // Setting default filter values.
+        $this->defaultfrom = new DateTime("now", core_date::get_server_timezone_object());
+        $this->defaultfrom->setTime(0, 0); // From the start of today.
+        $this->defaultto = new DateTime("now", core_date::get_server_timezone_object()); // Till now.
+        $ceilseconds = 60 - (int)$this->defaultto->format("s");
+        $this->defaultto->add(new DateInterval("PT{$ceilseconds}S")); // Ceil to nearest minute.
 
         parent::__construct();
     }
 
-    // Get form data, defaults returned if there's no submitted data
-    function get_data(): object {
-        $form_data = parent::get_data();
-        if(!$form_data) $form_data = (object)[
-            'from' => $this->default_from->getTimestamp(),
-            'to' => $this->default_to->getTimestamp(),
-            'course_id' => $this->default_course_id
-        ];
-        return $form_data;
+    /**
+     * Get form data, defaults returned if there's no submitted data
+     * @return stdClass
+     */
+    public function get_data(): stdClass {
+        $formdata = parent::get_data();
+        if (!$formdata) {
+            $formdata = (object)[
+                    'from' => $this->defaultfrom->getTimestamp(),
+                    'to' => $this->defaultto->getTimestamp(),
+                    'course_id' => $this->defaultcourseid,
+            ];
+        }
+        return $formdata;
     }
 
-    protected function definition()
-    {
+    /**
+     * Definition of form
+     */
+    protected function definition(): void {
         $mform = $this->_form;
 
-        // Date range selector filter (from/to)
+        // Date range selector filter (from/to).
         $mform->addElement('date_time_selector', 'from', 'From');
-        $mform->setDefault('from', $this->default_from->getTimestamp());
+        $mform->setDefault('from', $this->defaultfrom->getTimestamp());
 
         $mform->addElement('date_time_selector', 'to', 'To');
-        $mform->setDefault('to', $this->default_to->getTimestamp());
+        $mform->setDefault('to', $this->defaultto->getTimestamp());
 
-        // Course selector
+        // Course selector.
         $courses = ['all' => 'All'];
-        foreach (get_available_courses() as $course){
+        foreach (get_available_courses() as $course) {
             $courses[$course->id] = $course->shortname;
         }
         $mform->addElement('select', 'course_id', 'Course', $courses);
-        $mform->setDefault('course_id', $this->default_course_id);
+        $mform->setDefault('course_id', $this->defaultcourseid);
 
-        // Apply filter button
+        // Apply filter button.
         $mform->addElement('submit', 'apply_filter', 'Apply filter');
     }
 }
 
-// Check is course available for current user
-function is_course_available($course_id)
-{
-    $context = context_course::instance($course_id);
+/**
+ * Check if course is available for current user
+ * @param int $courseid
+ * @return bool
+ * @throws coding_exception
+ */
+function is_course_available(int $courseid): bool {
+    $context = context_course::instance($courseid);
     return has_capability('mod/bacs:viewany', $context);
 }
 
-// Get courses filtered by availability
-function get_available_courses(): array
-{
-    $available_courses = [];
+/**
+ * Get courses filtered by availability
+ * @return array
+ */
+function get_available_courses(): array {
+    $availablecourses = [];
 
     $courses = get_courses();
-    foreach($courses as $course) {
-        if (is_course_available($course->id))
-            $available_courses[] = $course;
+    foreach ($courses as $course) {
+        if (is_course_available($course->id)) {
+            $availablecourses[] = $course;
+        }
     }
 
-    return $available_courses;
+    return $availablecourses;
 }
 
-
-function get_filtered_submits($form_data): array
-{
+/**
+ * Get submits filtered by form data
+ * @param stdClass $formdata
+ * @return array
+ * @throws dml_exception
+ */
+function get_filtered_submits(stdClass $formdata): array {
     global $DB;
 
-    // Form SQL query with filtering
-    $course_id_filter = "";
-    if($form_data->course_id != 'all') $course_id_filter = " AND (course.id = $form_data->course_id)";
+    // Form SQL query with filtering.
+    $courseidfilter = "";
+    if ($formdata->course_id != 'all') {
+        $courseidfilter = " AND (course.id = $formdata->courseid)";
+    }
     $sql =
         "SELECT submit.id AS id, course.id AS course_id, submit_time
          FROM {bacs_submits} submit
-         JOIN {bacs} contest ON submit.contest_id = contest.id  
+         JOIN {bacs} contest ON submit.contest_id = contest.id
          JOIN {course} course ON contest.course = course.id
-         WHERE (submit_time BETWEEN $form_data->from AND $form_data->to) $course_id_filter";
+         WHERE (submit_time BETWEEN $formdata->from AND $formdata->to) $courseidfilter";
 
-    // Filter submits by availability
+    // Filter submits by availability.
     $submits = [];
     foreach ($DB->get_records_sql($sql) as $submit) {
-        if (is_course_available($submit->course_id))
+        if (is_course_available($submit->course_id)) {
             $submits[] = $submit;
+        }
     }
 
     return $submits;
 }
 
+/**
+ * Generate renderable bar chart object by given form data
+ * @param stdClass $formdata
+ * @return chart_bar
+ * @throws dml_exception
+ */
+function generate_chart(stdClass $formdata): chart_bar {
+    $labels = []; // Chart labels.
+    $submitsperhour = []; // Diagram values.
 
-function generate_chart($form_data): \core\chart_bar
-{
-    $labels = []; // Chart labels
-    $submits_per_hour = []; // Diagram values
-
-    // Iterate over all 24 hours
-    $day_period = new DatePeriod(
+    // Iterate over all 24 hours.
+    $dayperiod = new DatePeriod(
         new DateTime("00:00"),
         new DateInterval('PT1H'),
         new DateTime("24:00")
     );
-    foreach ($day_period as $date) {
-        $labels[] = $date->format("H:i"); // Hour xx:00
-        $submits_per_hour[$date->format("H")] = 0; //
+    foreach ($dayperiod as $date) {
+        $labels[] = $date->format("H:i"); // Hour xx:00.
+        $submitsperhour[$date->format("H")] = 0;
     }
 
-    // Gather filtered submits from DB
-    $submits = get_filtered_submits($form_data);
+    // Gather filtered submits from DB.
+    $submits = get_filtered_submits($formdata);
 
 
-    foreach ($submits as $submit){
-        $submits_per_hour[date("H", $submit->submit_time)]++;
+    foreach ($submits as $submit) {
+        $submitsperhour[date("H", $submit->submit_time)]++;
     }
 
-    $chart = new \core\chart_bar();
-    $series = new \core\chart_series("Submits", array_values($submits_per_hour));
+    $chart = new chart_bar();
+    $series = new chart_series("Submits", array_values($submitsperhour));
     $chart->set_labels($labels);
     $chart->add_series($series);
 
@@ -150,17 +220,17 @@ function generate_chart($form_data): \core\chart_bar
 
 
 
-// Render page
+// Render page.
 
-// Creating filter form instance
-$filter_form = new filter_form();
+// Creating filter form instance.
+$filterform = new filter_form();
 
 echo $OUTPUT->header();
 
-// Render filter form
-$filter_form->display();
+// Render filter form.
+$filterform->display();
 
-// Render diagram
-echo $OUTPUT->render(generate_chart($filter_form->get_data()));
+// Render diagram.
+echo $OUTPUT->render(generate_chart($filterform->get_data()));
 
 echo $OUTPUT->footer();
