65e80730b6779412e0708e4ecf1795dcc012cf04
Daniel Reker <sasha29072004@gmail.com>
2024-06-05 08:33:27 +0400

    Refactor controller script

--- submits_hourly_distribution.php
+++ submits_hourly_distribution.php
@@ -30,7 +30,7 @@ require_once(__DIR__ . '/chart_hourly_distribution.php');
 require_once(__DIR__ . '/submits_manager.php');
 
 // Set up page.
-$PAGE->set_url(new moodle_url('/bacs_charts/chart1.php'));
+$PAGE->set_url(new moodle_url('/bacs_charts/submits_hourly_distribution.php'));
 $PAGE->set_context(context_system::instance());
 $PAGE->set_heading("Submits amount distribution during the day");
 $PAGE->set_title("Sumbits during the day");
@@ -38,37 +38,27 @@ $PAGE->set_pagelayout("standard");
 
 require_login();
 
+// Controller logic.
 
-// Render page.
+$timezone = core_date::get_server_timezone_object(); // Preferred timezone.
+$submitsmanager = new submits_manager($timezone); // Submits manager.
+$filterform = new filter_form(submits_manager::get_available_courses(), $timezone); // Filter form.
 
-$timezone = core_date::get_server_timezone_object();
-
-// Creating filter form instance.
-$filterform = new filter_form(submits_manager::get_available_courses(), $timezone);
-
-echo $OUTPUT->header();
-
-// Render filter form.
-$filterform->display();
-
-
-
-// Render diagram.
+// Make chart.
+$chart = new chart_hourly_distribution();
 $filterstate = $filterform->get_filter_state();
-
-$submitsmanager = new submits_manager($timezone);
 $submits = $submitsmanager->get_available_filtered(
     $filterstate->from,
     $filterstate->to,
     $filterstate->courseid
 );
-
-$chart = new chart_hourly_distribution();
 $chart->add_date_times(
     array_map(fn($submit) => $submit->time, $submits),
     "Submits"
 );
+
+// Render page.
+echo $OUTPUT->header();
+$filterform->display();
 echo $chart->render_by($OUTPUT);
-
-
 echo $OUTPUT->footer();
